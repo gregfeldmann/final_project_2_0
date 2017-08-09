78d7a96eab297d1a2c3dcbbc1d94ae4da82a3eaa
Raghu Betina <raghu@firstdraft.com>
2017-08-09 15:32:50 +0000

    Whitelist columns in RecommendRating for ActiveAdmin

--- app/admin/recommend_rating.rb
+++ app/admin/recommend_rating.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register RecommendRating do
 
+ permit_params :rating_id, :friend_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
